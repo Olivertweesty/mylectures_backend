f00f6b8f48baf069dd04bc08617766a4d3221a7b
Olivertweesty <olivertweesty32@gmail.com>
2019-10-21 12:36:01 +0300

    Äddes app running

--- app.py
+++ app.py
@@ -7,5 +7,20 @@ app = Flask(__name__)
 def check_available():
     return "true"
 
+@app.route("/load_lessons",methods=["POST","GET"])
+def load_lessons():
+    data = [
+        {"day_of_week":"Monday",
+            "str_time":"12:00",
+            "st_time":"14:00",
+            "subject":"Design & Implementation",
+            "venue":"SCC 106",
+            "lec_name":"D. Waema",
+            "lec_tel":"254724487464",
+            "status":"cancelled",
+            "coodinates":"-1.675845,35.45435"}
+        ]
+    return jsonify(data)
+
 if __name__ == "__main__":
     app.run(debug=True,port=3000,host="0.0.0.0")
