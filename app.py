6efd04400d62aa24db9d6df8bc3e7670595cc938
Olivertweesty <olivertweesty32@gmail.com>
2019-10-21 12:08:21 +0300

    Äddes app running

--- app.py
+++ app.py
@@ -6,3 +6,6 @@ app = Flask(__name__)
 @app.route("/check_available",methods=["POST","GET"])
 def check_available():
     return "true"
+
+if __name__ == "__main__":
+    app.run(debug=True,port=3000,host="0.0.0.0")
